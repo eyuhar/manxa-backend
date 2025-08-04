23ff787de34d9dd5808ce1b9092c1db5420feab8
Eyyuep <eyuep.harputlu@gmail.com>
2025-08-04 23:01:34 +0200

    add cache headers to image proxy response

--- api/imageProxy.php
+++ api/imageProxy.php
@@ -31,4 +31,8 @@ $mimeType = $finfo->buffer($imageData) ?: 'application/octet-stream';
 header("Content-Type: $mimeType");
 header("Content-Length: " . strlen($imageData));
 
+// Set cache headers – 1 hour
+header("Cache-Control: public, max-age=3600");
+header("Expires: " . gmdate('D, d M Y H:i:s', time() + 3600) . " GMT");
+
 echo $imageData;
